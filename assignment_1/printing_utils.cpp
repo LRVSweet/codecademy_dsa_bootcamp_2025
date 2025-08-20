6226b7a3b214324ff3e7185445126fa604db824f
lsweet <logan.malf22@gmail.com>
2025-08-20 10:59:40 -0300

    Add printing_utils cpp and header file.

---
+++ assignment_1/printing_utils.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+#include "printing_utils.hpp"
+
+using namespace std;
+
+void print_char (int count, char char_to_print)
+{
+    for (int i = 0; i < count; i++)
+    {
+        cout << char_to_print;
+    }
+}
---
+++ assignment_1/printing_utils.hpp
@@ -0,0 +1,5 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void print_char (int count, char char_to_print);
