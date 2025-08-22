f0ce795c617689276114a8cfa6879c7226f01bf7
lsweet <logan.malf22@gmail.com>
2025-08-22 00:53:40 -0300

    Add final code for 13
    Previous commit with 13 erroneously featured the .out file, not the .cpp file.

--- assignment_1/1.13.cpp
+++ assignment_1/1.13.cpp
@@ -2,19 +2,50 @@
 
 using namespace std;
 
+vector<int> get_palindrome_sequence (int input)
+{
+    int max_iterations = input + (input - 1);
+    vector<int> result_vec;
+
+    for (int i = 0; i < max_iterations; i++)
+    {
+        int result = abs((input - 1) - i) + 1;
+        result_vec.push_back(result);
+    }
+
+    return result_vec;
+}
+
 int main ()
 {
     cout << "Please enter a number." << endl;
     int input;
     cin >> input;
 
-    int size = 2 * input - 1;
+    vector<int> palindrome_vec = get_palindrome_sequence(input);
 
-    for (int row = 0; row < size; row++)
+    // palindrome_vec serves a dual purpose:
+    // 1. A list of the min values for each row.
+    // 2. A row "template" that is compared against the min.
+    
+    // The outer loop provides each row's min value.
+    for (int row_min : palindrome_vec)
     {
-        for (int col = 0; col < size; col++)
+        // The inner loop produces the result by comparison against the min.
+        for (int element : palindrome_vec)
         {
-        
+            if (element < row_min)
+            {
+                cout << row_min << " ";
+            }
+            else
+            {
+                cout << element << " ";
+            }
         }
+
+        cout << endl;
     }
+
+    return 0;
 }
