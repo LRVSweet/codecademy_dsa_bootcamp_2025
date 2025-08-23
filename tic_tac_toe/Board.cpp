156c848450aa303c827c7c8d14c1834b92ae7708
lsweet <logan.malf22@gmail.com>
2025-08-23 19:38:14 -0300

    Begin adding method definitions.

--- tic_tac_toe/Board.cpp
+++ tic_tac_toe/Board.cpp
@@ -1,32 +1,138 @@
 #include <iostream>
 #include <vector>
+
+#include "Board.hpp"
+
 using namespace std;
 
-class Board
+vector<vector<char>> Board::empty_board = 
 {
-private:
-    static vector<vector<char>> empty_board =
-    {
-        {' ', ' ', ' '},
-        {' ', ' ', ' '},
-        {' ', ' ', ' '}
-    }
-
-    vector<vector<char>> board;
-
-public:
-    Board(vector<vector<char>> input_board = Board::empty_board)
-    {
-        board = input_board;
-    }
-    
-    Board makeMove(int horizontal, int vertical, int player)
-    {
-        
-    }
-    
-    
-    void storeMove(int horizontal, int vertical, int player);
-    vector<vector<char>> getBoard();
-    int checkForCompletion();
+    {' ', ' ', ' '},
+    {' ', ' ', ' '},
+    {' ', ' ', ' '}
 };
+
+
+Board::Board(vector<vector<char>> input_board = Board::empty_board)
+{
+    board = input_board;
+}
+    
+void Board::storeMove(int horizontal, int vertical, char player)
+{
+    this->board.at(vertical).at(horizontal) = player;
+}
+
+Board Board::makeMove(int horizontal, int vertical, char player)
+{
+    Board new_board = *this;
+    new_board.board.at(vertical).at(horizontal) = player;
+    return new_board;
+
+}
+
+vector<vector<char>> Board::getBoard()
+{
+    return this->board;
+}
+
+int Board::checkHorizontalComp()
+{
+
+    for (vector<char> row : this->board)
+    {
+        char first_char = row.at(0);
+        int matching_char_count = 0;
+        
+        for (char element : row)
+        {
+            if (element == ' ') break;
+            else if (element != first_char) break;
+            else matching_char_count++;
+        }
+
+        if (matching_char_count == 3 && first_char == 'X')
+        {
+            return 1;
+        }
+        else if (matching_char_count == 3 && first_char == 'Y')
+        {
+            return 2;
+        }
+    }
+
+    return 0;
+}
+
+int Board::checkVerticalComp()
+{
+    for (int i = 0; i < 3; i++)
+    {
+        char first_row_val = this->board.at(0).at(i);
+        char second_row_val = this->board.at(1).at(i);
+        char third_row_val = this->board.at(2).at(i);
+
+        if (first_row_val == ' ' || second_row_val == ' ' || third_row_val == ' ')
+        {
+            continue;
+        }
+        else if (first_row_val == 'X' && second_row_val == 'X' && third_row_val == 'X')
+        {
+            return 1;
+        }
+        else if (first_row_val == 'Y' && second_row_val == 'Y' && third_row_val == 'Y')
+        {
+            return 2;
+        }  
+    }
+
+    return 0;
+}
+
+int Board::checkDiagonalComp()
+{
+    vector<char> main_diagonal;
+    
+    for (int i = 0; i < this->board.size(); i++)
+    {
+        main_diagonal.push_back(this->board.at(i).at(i));
+    }
+    
+    for (char element : main_diagonal)
+    {
+
+    }
+}
+
+int Board::checkForCompletion(int moves_elapsed)
+{
+    int result = 0;
+    int empty_slots = 0;
+
+    // --- Step 1: Check for horizontal completions ---
+    for (vector<char> row : this->board)
+    {
+        char first_char = row.at(0);
+        int matching_char_count = 0;
+        
+        for (char element : row)
+        {
+            if (element == ' ') break;
+            else if (element != first_char) break;
+            else matching_char_count++;
+        }
+
+        if (matching_char_count == 3 && first_char == 'X')
+        {
+            result = 1;
+            return result;
+        }
+        else if (matching_char_count == 3 && first_char == 'Y')
+        {
+            result = 2;
+            return result;
+        }
+    }
+
+    // --- Step 2
+}
--- tic_tac_toe/Board.hpp
+++ tic_tac_toe/Board.hpp
@@ -2,16 +2,76 @@
 
 #include <vector>
 
+/**
+ * Represents a tic-tac-toe board
+ */
 class Board
 {
 private:
+    /**
+     * empty_board - Stores a 2-d vector of an empty Board.
+     */
     static std::vector<std::vector<char>> empty_board;
+    
+    /**
+     * board - Stores a 2-d vector containing the current Board.
+     */
     std::vector<std::vector<char>> board;
 
+    /**
+     * Checks for homogenous non-empty rows.
+     * If a homogenous row of 'X' is found, returns 1
+     * If a homogenous row of 'Y' is found, returns 2
+     * Else, returns 0
+     */
+    int checkHorizontalComp();
+
+    /**
+     * Checks for homogenous non-empty columns.
+     * If a homogenous column of 'X' is found, returns 1
+     * If a homogenous column of 'Y' is found, returns 2
+     * Else, returns 0
+     */
+    int checkVerticalComp();
+
+    /**
+     * Checks for homogenous non-empty diagonals.
+     * If a homogenous diagonal of 'X' is found, returns 1
+     * If a homogenous diagonal of 'Y' is found, returns 2
+     * Else, returns 0
+     */
+    int checkDiagonalComp();
+
 public:
-    Board();
-    Board makeMove(int horizontal, int vertical, int player);
-    void storeMove(int horizontal, int vertical, int player);
+    
+    /**
+     * Contructor - Accepts an optional board parameter in the form of a 2-d vector.
+     * This parameter defaults to emtpy_board.
+     */
+    Board(std::vector<std::vector<char>> input_board);
+    
+    /**
+     * Return a new Board based on the current board plus the move specified in the parameters.
+     * makeMove() does not mutate the current board.
+     */
+    Board makeMove(int horizontal, int vertical, char player);
+    
+    /**
+     * Adds the specified move to the current board.
+     */
+    void storeMove(int horizontal, int vertical, char player);
+    
+    /**
+     * Returns a 2-d vector representing the current board.
+     */
     std::vector<std::vector<char>> getBoard();
-    int checkForCompletion();
+    
+    /**
+     * Returns an integer representing the completion state of the current board:
+     * 0: Incomplete (there are still legal moves to make).
+     * 1: Player 1 (X) wins.
+     * 2: Play 2 (O) wins.
+     * 3: Cat's game or tie.
+     */
+    int checkForCompletion(int moves_elapsed);
 };
