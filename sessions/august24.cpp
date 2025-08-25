74307c296573e22b12a52fd296f667c33cd140f3
lsweet <logan.malf22@gmail.com>
2025-08-24 22:34:07 -0300

    Correct erroneous commit (prev)

--- sessions/august24.cpp
+++ sessions/august24.cpp
@@ -1,7 +1,9 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
+// Attention - Did not terminate
 vector<int> searchSort (vector<int> &input_array)
 {
     for (int i = 0; i < input_array.size(); i++)
@@ -70,6 +72,7 @@ vector<int> bubbleSortRecursive (vector<int> input_array, int endIdx)
 */
 
 /*
+// ATTENTION - Complete Implementation
 void insertionSort (vector<int> &input_array)
 {
     vector<int> new_array = {input_array.at(0)};
@@ -91,32 +94,74 @@ void mergeTwoSortedArrays(vector<int> &input_array, int start_index, int mid, in
 
     int i = start_index, j = mid + 1;
 
-    while(i <= mid && j <= end_index)
+    while (i <= mid && j <= end_index)
     {
+        if (input_array.at(i) < input_array.at(j))
+        {
+            temp_vector.push_back(input_array.at(i++));
+        }
+        else
+        {
+            temp_vector.push_back(input_array.at(j++));
+        }
+    }
 
+    for (i; i<= mid; i++)
+    {
+        temp_vector.push_back(input_array.at(i));
+    }
+
+    for (j; j<= end_index; j++)
+    {
+        temp_vector.push_back(input_array.at(j));
+    }
+
+    // Copy temp_vector into input_array
+    int k = start_index;
+    for (int element : temp_vector)
+    {
+        input_array[k++] = element;
     }
 }
 
-vector<int> mergeSort (vector<int> &input_array, int start_index, int end_index)
+void mergeSort (vector<int> &input_array, int start_index, int end_index)
 {
     // Base case
-    if (input_array.size() == 1)
-    {
-        return input_array;
-    }
+    if (start_index >= end_index)
+    return;
 
     // Recursive Step
     int mid = start_index + ((end_index - start_index) / 2);
     mergeSort(input_array, start_index, mid);
     mergeSort(input_array, mid + 1, end_index);
 
-
+    mergeTwoSortedArrays(input_array, start_index, mid, end_index);
 }
 
 // Helper function for quickSort
-void partition(vector<int> arr, int startIdx, int endIdx)
+int partition(vector<int> &arr, int startIdx, int endIdx)
 {
+    int pivot = arr.at(startIdx);
 
+    int lower_part_index = startIdx;
+    int higher_part_index = endIdx;
+
+    while (lower_part_index < higher_part_index)
+    {
+        while (arr.at(lower_part_index) <= pivot && lower_part_index < endIdx)
+        lower_part_index++;
+
+        while (arr.at(higher_part_index) > pivot && higher_part_index > startIdx)
+        higher_part_index--;
+
+        if (lower_part_index < higher_part_index)
+        swap(arr[lower_part_index], arr[higher_part_index]);
+    }
+
+    // Why does it have to be the higher_part_index that is swapped?
+    swap(arr[startIdx], arr[higher_part_index]);
+
+    return higher_part_index;
 }
 
 void quickSort(vector<int> &arr, int startIdx, int endIdx)
@@ -126,14 +171,13 @@ void quickSort(vector<int> &arr, int startIdx, int endIdx)
 
     int partitionIdx = partition(arr, startIdx, endIdx);
     quickSort(arr, startIdx, partitionIdx - 1);
-    quickSort(arr, partitionIdx + 1, endIdx)
+    quickSort(arr, partitionIdx + 1, endIdx);
 }
 
 int main ()
 {
-    vector<int> input_array = {7, 5, 0, 2};
-    // searchSort(input_array);
-    bubbleSort(input_array);
+    vector<int> input_array = {7, 5, 9, 2, 10, 88, 17, -1, 7};
+    mergeSort(input_array, 0, input_array.size() - 1);
 
     cout << "Printing sorted array" << endl;
 
